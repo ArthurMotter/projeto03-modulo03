e9c61d69c069b3fb884b7bcf6b62ca7ae9da4b1b
Arthur Motter <arthur.motter@fatec.sp.gov.br>
2025-07-29 10:55:37 -0300

    put and get by id endpoints done

--- seller-backend/src/main/java/com/abutua/seller_backend/controllers/SellerController.java
+++ seller-backend/src/main/java/com/abutua/seller_backend/controllers/SellerController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,13 +26,21 @@ public class SellerController {
     @Autowired
     private SellerService service;
 
+    // GET All
     @GetMapping
     public ResponseEntity<List<Seller>> findAll() {
         List<Seller> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 
-    // POST Endpoint
+    // GET by Id
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Seller> findById(@PathVariable long id) {
+        Seller seller = service.findById(id);
+        return ResponseEntity.ok(seller);
+    }
+
+    // POST
     @PostMapping
     public ResponseEntity<Seller> insert(@Valid @RequestBody SellerDTO sellerDTO) {
         Seller newSeller = service.insert(sellerDTO);
@@ -39,5 +49,12 @@ public class SellerController {
                     .buildAndExpand(newSeller.getId()).toUri();
         
         return ResponseEntity.created(uri).body(newSeller);
+    }
+
+    // PUT
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Seller> update(@PathVariable long id, @Valid @RequestBody SellerDTO sellerDTO) {
+        Seller updatedSeller = service.update(id, sellerDTO);
+        return ResponseEntity.ok(updatedSeller);
     }
 }
--- seller-backend/src/main/java/com/abutua/seller_backend/services/SellerService.java
+++ seller-backend/src/main/java/com/abutua/seller_backend/services/SellerService.java
@@ -1,12 +1,16 @@
 package com.abutua.seller_backend.services;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.abutua.seller_backend.dtos.SellerDTO;
 import com.abutua.seller_backend.models.Seller;
 import com.abutua.seller_backend.repositories.SellerRepository;
+import com.abutua.seller_backend.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class SellerService {
@@ -18,6 +22,12 @@ public class SellerService {
         return repository.findAll();
     }
 
+    // GET by ID
+    public Seller findById(long id) {
+        Optional<Seller> result = repository.findById(id);
+        return result.orElseThrow(() -> new ResourceNotFoundException("Vendedor não encontrado"));
+    }
+
     // DTO converter
     public Seller insert(SellerDTO sellerDTO) {
         Seller entity = new Seller();
@@ -27,5 +37,21 @@ public class SellerService {
         entity.setGender(sellerDTO.getGender());
         
         return repository.save(entity);
+    }
+
+    // Update
+    public Seller update(long id, SellerDTO sellerDTO) {
+        try {
+            Seller entity = repository.getReferenceById(id);
+            
+            entity.setName(sellerDTO.getName());
+            entity.setSalary(sellerDTO.getSalary());
+            entity.setBonus(sellerDTO.getBonus());
+            entity.setGender(sellerDTO.getGender());
+
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Vendedor não encontrado");
+        }
     }
 }
