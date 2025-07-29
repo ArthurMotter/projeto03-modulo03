f1f78e3bd2dbd9408c3096dbeb8455a9eb2af6cf
Arthur Motter <arthur.motter@fatec.sp.gov.br>
2025-07-29 10:59:26 -0300

    delete endpoint done

--- seller-backend/src/main/java/com/abutua/seller_backend/controllers/SellerController.java
+++ seller-backend/src/main/java/com/abutua/seller_backend/controllers/SellerController.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -56,5 +57,12 @@ public class SellerController {
     public ResponseEntity<Seller> update(@PathVariable long id, @Valid @RequestBody SellerDTO sellerDTO) {
         Seller updatedSeller = service.update(id, sellerDTO);
         return ResponseEntity.ok(updatedSeller);
+    }
+
+    // DELETE
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- seller-backend/src/main/java/com/abutua/seller_backend/services/SellerService.java
+++ seller-backend/src/main/java/com/abutua/seller_backend/services/SellerService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.abutua.seller_backend.dtos.SellerDTO;
@@ -52,6 +53,18 @@ public class SellerService {
             return repository.save(entity);
         } catch (EntityNotFoundException e) {
             throw new ResourceNotFoundException("Vendedor não encontrado");
+        }
+    }
+
+    // Delete
+    public void delete(long id) {
+        findById(id); 
+
+        try {
+            repository.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e) {
+            throw new RuntimeException("Violação de integridade de dados");
         }
     }
 }
